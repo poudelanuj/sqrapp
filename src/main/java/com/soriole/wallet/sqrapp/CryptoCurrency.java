0555a9ea0ab2fd8a6eb59a3c4f749fa6e13c01d5
sireto <np.sandip.pandey@gmail.com>
2017-12-26 10:38:10 +0100

    Added CryptoCurrency interface

---
+++ src/main/java/com/soriole/wallet/sqrapp/CryptoCurrency.java
@@ -0,0 +1,43 @@
+package com.soriole.wallet.sqrapp;
+
+/**
+ * Defines the basic interface to be implemented by every supported cryptocurrencies.
+ */
+public interface CryptoCurrency {
+
+    /**
+     * Creates a new seed to be used for generating private keys
+     * @return seed value
+     */
+    byte[] newSeed();
+
+    /**
+     * Creates a new private key
+     * @return private key
+     */
+    byte[] newPrivateKey();
+
+    /**
+     * Creates a new private key using the provided seed value
+     * @param seed seed value
+     * @return private key
+     */
+    byte[] newPrivateKey(byte[] seed);
+
+    /**
+     * Creates a new private key of given index using the provided seed value
+     * @param seed seed value
+     * @param index index of private key
+     * @return private key
+     */
+    byte[] newPrivateKey(byte[] seed, int index);
+
+    /**
+     * Returns public key corresponding to the given private key
+     * @param privateKey private key
+     * @return public key
+     */
+    byte[] publicKey(byte[] privateKey);
+
+
+}
