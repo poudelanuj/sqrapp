4c3ba3f6b935fbccd8a840673ed6d6a213ee37d8
sireto <np.sandip.pandey@gmail.com>
2017-12-26 11:58:50 +0100

    Added mock implementations for Cardano, Monero, Neo, Stratis and Waves

---
+++ src/main/java/com/soriole/wallet/sqrapp/neo/Neo.java
@@ -0,0 +1,30 @@
+package com.soriole.wallet.sqrapp.neo;
+
+import com.soriole.wallet.sqrapp.CryptoCurrency;
+
+public class Neo implements CryptoCurrency{
+    @Override
+    public byte[] newSeed() {
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] newPrivateKey() {
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] newPrivateKey(byte[] seed) {
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] newPrivateKey(byte[] seed, int index) {
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] publicKey(byte[] privateKey) {
+        return new byte[0];
+    }
+}
---
+++ src/main/java/com/soriole/wallet/sqrapp/waves/Waves.java
@@ -0,0 +1,30 @@
+package com.soriole.wallet.sqrapp.waves;
+
+import com.soriole.wallet.sqrapp.CryptoCurrency;
+
+public class Waves implements CryptoCurrency{
+    @Override
+    public byte[] newSeed() {
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] newPrivateKey() {
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] newPrivateKey(byte[] seed) {
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] newPrivateKey(byte[] seed, int index) {
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] publicKey(byte[] privateKey) {
+        return new byte[0];
+    }
+}
